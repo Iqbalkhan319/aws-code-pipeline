07189b683284b49c94e85111bd249f9a11633a85
Md. Masud Parvaze <141165442+parvaze-masud@users.noreply.github.com>
2023-10-08 14:36:28 +0600

    Update CodeDeploy agent.sh

--- CodeDeploy agent.sh
+++ CodeDeploy agent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install ruby-full
-sudo apt install wget
+sudo apt install ruby-full -y
+sudo apt install wget -y
+cd /home/ubuntu
 wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent status
-sudo service codedeploy-agent restart
-sudo service codedeploy-agent status
+sudo service codedeploy-agent start
